f759f7c8c1467a8d925329d907debb26218cc154
Chartwalker <bastelmaus@chartwalker.net>
2023-08-12 22:17:42 +0200

    update

---
+++ develop/src/gui/volarodeo.c
@@ -0,0 +1,9 @@
+#include <volarodeo/config.h>
+#include <volarodeo/gui/volarodeo.h>
+
+int main(int argc, char** argv){
+  printf("\n\t%s\n\n\t%s\n\n",TSC_PRJG_VERSION_S,TSC_PRJG_COMP_S);
+ 
+  return(0);
+  
+}   
