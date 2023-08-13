8c64c8d5bd07801d69822ed60be0e504e1f6b83c
Chartwalker <bastelmaus@chartwalker.net>
2023-08-13 10:14:57 +0200

    update pl 10

--- develop/src/gui/volarodeo.c
+++ develop/src/gui/volarodeo.c
@@ -2,28 +2,36 @@
 #include <volarodeo/gui/volarodeo.h>
 #include <gtk/gtk.h>
 
-static gint delete_Event(GtkWidget * widget, GdkEvent event, gpointer daten){
-  g_print ("Das Fenster wird zerstoert!\n");
-  /* Nur mit FALSE wird die Anwendung wirklich beendet */
+static gint delete_Event(GtkWidget * widget, GdkEvent event, gpointer data){
+#ifdef __TSC_PRJG_LANG_DE__
+  g_print ("Das Anwendungsfenster wird geschlossen.\n");
+#endif
+#ifdef __TSC_PRJG_LANG_EN__
+  g_print ("The application window will be closed.\n");
+#endif
   return FALSE;
 }
 
-static void end (GtkWidget * widget, gpointer daten) {
-  g_print ("Und tschuess!\n");
-  /* Die Verarbeitungsschleife beenden */
+static void end (GtkWidget * widget, gpointer data) {
+#ifdef __TSC_PRJG_LANG_DE__
+  g_print ("Die App wird beendet.\n");
+#endif
+#ifdef __TSC_PRJG_LANG_EN__
+  g_print ("The app will exit.\n");
+#endif
   gtk_main_quit ();
 }
 
 int main (int argc, char **argv) {
   GtkWindow *win;
   GdkPixbuf *pic;
-  /* 1. Die Umgebung initialisieren */
+  /* init the environment */
   gtk_init (&argc, &argv);
 
-  /* 2. Die Widgets erzeugen */
-  /* Eine Grafik in einen Pixbuf laden */
-  pic = gdk_pixbuf_new_from_file ("icon/at-work.gif", NULL);
-  /* 2a. Fenster mit den folgenden Eigenschaften ... anlegen */
+  /* creating widgets */
+  /* load image to Pixbuf */
+  pic = gdk_pixbuf_new_from_file ("icon/rattelsnake.png", NULL);
+  /* set defaults */
   win = g_object_new (GTK_TYPE_WINDOW,
                       "title", TSC_PRJG_VERSION_S,
                       "default-width", 1000,
@@ -31,16 +39,21 @@ int main (int argc, char **argv) {
                       "resizable", TRUE,
                       "window-position", GTK_WIN_POS_CENTER,
                       "border-width", 5, "icon", pic, NULL);
-  /* 3. Signalhandler (Callback-Funktion) einrichten  */
+  /* create callbacks  */
   g_signal_connect ( win, "delete-event",
                      G_CALLBACK (delete_Event), NULL);
   g_signal_connect ( win, "destroy",
                      G_CALLBACK (end), NULL);
-  /* 4. Hier gibt es noch nichts zum Packen */
-  /* 5. (Widgets-)Fenster anzeigen */
+
+  /* show widget */
   gtk_widget_show_all (GTK_WIDGET (win));
-  /* 6. Hauptschleife von gtk-Verarbeitungsschleife */
+  /* main loop */
   gtk_main ();
-  g_print ("Die GTK-Hauptschleife wurde beendet\n");
+#ifdef __TSC_PRJG_LANG_DE__
+  g_print ("Task wird beendet.\n");
+#endif
+#ifdef __TSC_PRJG_LANG_EN__
+  g_print ("Task is terminated.\n");
+#endif
   return 0;
 }
