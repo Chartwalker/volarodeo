70b108748a7da816d85b85d8e85c3c6876c1df67
Chartwalker <bastelmaus@chartwalker.net>
2024-04-02 23:58:09 +0200

    moved to ./lib

--- develop/src/libsymbiot/bitops.h
+++
@@ -1,9 +0,0 @@
-// usefull bitfield standard from clang FAQ
-#include <limits.h>
-
-#define BITMASK(b) (1 << ((b) % CHAR_BIT))
-#define BITSLOT(b) ((b) / CHAR_BIT)
-#define BITSET(a, b) ((a)[BITSLOT(b)] |= BITMASK(b))
-#define BITCLEAR(a, b) ((a)[BITSLOT(b)] &= ~BITMASK(b))
-#define BITTEST(a, b) ((a)[BITSLOT(b)] & BITMASK(b))
-#define BITNSLOTS(nb) ((nb + CHAR_BIT - 1) / CHAR_BIT)
